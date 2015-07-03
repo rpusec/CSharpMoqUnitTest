d4a1f12a61f2bbf886490545ff8463f9118f711e
rpusec <roman.pusec@gmail.com>
2015-07-03 10:45:46 +0200

    Added a rating level enumeration for Customers.

---
+++ MoqUnitTest/BookOrderingSystem/Model/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoqUnitTest.BookOrderingSystem.Model
+{
+    public enum CustomerLevel { BRONZE, SILVER, GOLD }
+
+    public class Customer
+    {
+        public IList<Model.BookCopy> OrderedBooks { get; private set; }
+
+        public Customer() 
+        {
+            OrderedBooks = new List<Model.BookCopy>();
+        }
+    }
+}
